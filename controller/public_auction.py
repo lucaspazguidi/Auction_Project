c6088f4424e5f5eb69e6f06e19558409ecc0bbf6
Lucas Guidi <137838917+lucaspazguidi@users.noreply.github.com>
2026-03-10 18:19:47 -0300

    arrumando data para utc america

--- controller/public_auction.py
+++ controller/public_auction.py
@@ -2,9 +2,9 @@ from flask import render_template, redirect, url_for, session, Blueprint, reques
 from models.auction import Auction
 from models.db import db
 from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
 
 public_auction_bp = Blueprint("public_auction", __name__)
-
 
 @public_auction_bp.route("/public_auction", methods=["GET", "POST"])
 def create_public_auction():
@@ -19,13 +19,20 @@ def create_public_auction():
         start_date = request.form.get("start_date")
         end_date = request.form.get("end_date")
 
-        # Converter datas para datetime com timezone
-        st_date = datetime.strptime(start_date, "%Y-%m-%dT%H:%M").replace(tzinfo=timezone.utc)
-        en_date = datetime.strptime(end_date, "%Y-%m-%dT%H:%M").replace(tzinfo=timezone.utc)
+        # timezone do Brasil
+        br_tz = ZoneInfo("America/Sao_Paulo")
+
+        # converter string para datetime local
+        st_date = datetime.strptime(start_date, "%Y-%m-%dT%H:%M").replace(tzinfo=br_tz)
+        en_date = datetime.strptime(end_date, "%Y-%m-%dT%H:%M").replace(tzinfo=br_tz)
+
+        # converter para UTC antes de salvar
+        st_date = st_date.astimezone(timezone.utc)
+        en_date = en_date.astimezone(timezone.utc)
 
         now = datetime.now(timezone.utc).replace(second=0, microsecond=0)
 
-        # Definir situação inicial
+        # definir situação inicial
         if st_date > now:
             situation = "scheduled"
         else:
