33fe3dfc36d8d11c3ed69ccf8bad89489b95f4aa
Lucas Guidi <137838917+lucaspazguidi@users.noreply.github.com>
2026-03-10 17:22:28 -0300

    alterando rota publica de leilao

--- controller/public_auction.py
+++ controller/public_auction.py
@@ -2,53 +2,63 @@ from flask import render_template, redirect, url_for, session, Blueprint, reques
 from models.auction import Auction
 from models.db import db
 from datetime import datetime
-from scheduler.jobs import schedule_close, schedule_open
+from scheduler.jobs import close_auction, public_auction as open_auction
 
 public_auction_bp = Blueprint("public_auction", __name__)
 
+
 @public_auction_bp.route("/public_auction", methods=["GET", "POST"])
-def public_auction():
+def create_public_auction():
+
     if request.method == "POST":
-        item = int(request.form.get("item"))   # Get item's id 
+
+        item = int(request.form.get("item"))
         price = float(request.form.get("price"))
         buy_now = float(request.form.get("buy_now"))
         min_bid = float(request.form.get("min_bid"))
+
         start_date = request.form.get("start_date")
         end_date = request.form.get("end_date")
 
-        now = datetime.now()     # Get current server's datetime
-        date_now = now.replace(second=0, microsecond=0)   # Save only year/month/day/hour/min
+        # Convertendo datas
+        st_date = datetime.strptime(start_date, "%Y-%m-%dT%H:%M")
+        en_date = datetime.strptime(end_date, "%Y-%m-%dT%H:%M")
 
-        st_date = datetime.strptime(start_date, "%Y-%m-%dT%H:%M")  # Convert string into date obj
+        now = datetime.now().replace(second=0, microsecond=0)
 
-        if st_date > date_now:
+        # Definir situação inicial
+        if st_date > now:
             situation = "scheduled"
         else:
             situation = "active"
 
-        # Creating new Auction instance
+        # Criar leilão
         new_auction = Auction(
-            user_id = session.get("user_id"),
-            item_id = item,
-            start_date = st_date,
-            end_date = end_date,
-            min_bid = min_bid,
-            starting_price = price,
-            situation = situation,
-            buy_now = buy_now
+            user_id=session.get("user_id"),
+            item_id=item,
+            start_date=st_date,
+            end_date=en_date,
+            min_bid=min_bid,
+            starting_price=price,
+            situation=situation,
+            buy_now=buy_now
         )
 
-        # add new instance and save in DB
         db.session.add(new_auction)
         db.session.commit()
 
+        # Se o leilão começa no futuro
         if new_auction.situation == "scheduled":
-            # Call the function to schedule the auction opening and closing
-            schedule_open(new_auction)
-            schedule_close(new_auction)
+
+            # Agenda abertura
+            open_auction(new_auction.auction_id)
+
+            # Agenda fechamento
+            close_auction(new_auction.auction_id)
+
         else:
-            # Call the function to schedule the auction closing
-            schedule_close(new_auction)
+            # Apenas agenda fechamento
+            close_auction(new_auction.auction_id)
 
         return redirect(url_for("home.home"))
 
